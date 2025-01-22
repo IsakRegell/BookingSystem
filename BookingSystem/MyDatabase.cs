e4e63a0bbbe920a3f10618a13948dc8ba57decdb
MDaskalou <mikael.daskalou@hotmail.com>
2025-01-22 12:55:56 +0100

    Uppdaterat Console app.  Bookingsklass gjort

--- BookingSystem/MyDatabase.cs
+++ BookingSystem/MyDatabase.cs
@@ -5,11 +5,14 @@ namespace BookingSystem
     public class MyDatabase : DbContext
     {
         public DbSet<Student> Student { get; set; } // Table for students
+        public DbSet<Bookings> Bookings { get; set; } //Table for Bookings
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // Connection string for the database
             optionsBuilder.UseSqlServer(@"Server=DESKTOP-SFTN8V0\SQLEXPRESS;Database=BookingSystem;Trusted_Connection=True;TrustServerCertificate=True;");
         }
+
+
     }
 }
