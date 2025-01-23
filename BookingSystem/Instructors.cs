b2685799cc28800c9a49ca090a630977ec0d8c43
TomasSteifo <Steifotomas@gmail.com>
2025-01-23 13:52:17 +0100

    snälla

---
+++ BookingSystem/Instructors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem
+{
+    public class Instructors
+    {
+        public int InstructorId { get; set; } // Primary Key
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Style { get; set; }
+    }
+}
+}
